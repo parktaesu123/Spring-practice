9dc66e911a9af64b53a09b3ea98bdead9b1e3cc9
troy96475 <taesupark0504@gmail.com>
2024-12-25 23:53:10 +0900

    feat :: userRepository

---
+++ src/main/java/com/example/demo/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.user.repository;
+
+import com.example.demo.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAccountId(String accountId);
+}
