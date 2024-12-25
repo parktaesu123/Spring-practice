87d33c00a21dc3c5d68477ca6a0c4216b7814c3d
troy96475 <taesupark0504@gmail.com>
2024-12-25 23:54:27 +0900

    feat :: refreshTokenRepository

---
+++ src/main/java/com/example/demo/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.auth.repository;
+
+import com.example.demo.domain.auth.domain.RefreshToken;
+import com.example.demo.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+
+}
