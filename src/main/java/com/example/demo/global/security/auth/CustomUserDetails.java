229e4f97310cd51ee05c36607e7491c2247e24e1
troy96475 <taesupark0504@gmail.com>
2024-12-25 23:50:43 +0900

    feat :: 인증과정

---
+++ src/main/java/com/example/demo/global/security/auth/CustomUserDetails.java
@@ -0,0 +1,48 @@
+package com.example.demo.global.security.auth;
+
+import com.example.demo.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public record CustomUserDetails(User user) implements UserDetails {
+    @Override
+    public String getUsername() {   //로그인 아이디 역할을 하는 값 반환
+        return user.getAccountId(); //이메일을 기반 사용자 인증 설정
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {    //사용자의 권한 목록을 반환
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole().toString())));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/demo/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.demo.global.security.auth;
+
+import com.example.demo.domain.user.domain.User;
+import com.example.demo.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) {
+        User user = userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+        return new CustomUserDetails(user);
+    }
+}
